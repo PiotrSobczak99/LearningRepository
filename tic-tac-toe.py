027b8774664661a103f616d0de984c6ee84422e8
Wojciech Sobczak <wojcienty@gmail.com>
2021-05-15 23:11:47 +0200

    Little bit of refactor to get rid of global variable.

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,6 +1,5 @@
 import random
 
-
 def circle_or_cross():
     while True:
         decision = input("Player, do you want to play as cross or circle?: ")
@@ -23,27 +22,31 @@ def does_player_start():
 
 
 def print_instruction_board():
-    # print("0 | 1 | 2")
-    # print("---------")
-    # print("3 | 4 | 5")
-    # print("---------")
-    # print("6 | 7 | 8\n")
-    pass
-
+    print("Use indices as shown below:")
+    print("0 | 1 | 2")
+    print("---------")
+    print("3 | 4 | 5")
+    print("---------")
+    print("6 | 7 | 8")
+    print()
 
 
 def print_current_board(fields_array):
-    print("{} | {} | {}".format(fields_array[0],fields_array[1],fields_array[2]))
+    print("{} | {} | {}".format(fields_array[0], fields_array[1], fields_array[2]))
     print("---------")
-    print("{} | {} | {}".format(fields_array[3],fields_array[4],fields_array[5]))
+    print("{} | {} | {}".format(fields_array[3], fields_array[4], fields_array[5]))
     print("---------")
-    print("{} | {} | {}\n".format(fields_array[6],fields_array[7],fields_array[8]))
+    print("{} | {} | {}".format(fields_array[6], fields_array[7], fields_array[8]))
+    print()
 
 def get_player_move():
-    print_instruction_board()
     while True:
-        player_move = input("choose a number from 0 to 8 where you want to move: ")
-        player_move = int(player_move)
+        player_move = input("Choose a number from 0 to 8 where you want to move: ")
+        try:
+            player_move = int(player_move)
+        except ValueError:
+            print("Input must be a number!")
+            continue
         if player_move <= 8 and player_move >= 0:
             return player_move
         else:
@@ -54,123 +57,94 @@ def get_ai_move(fields_array):
         random_number = random.randint(0,8)
         if fields_array[random_number] == " ":
             return random_number
-    
         
-def ai_symbol_set(user_symbol):
+def get_ai_symbol(user_symbol):
     if user_symbol == "X":
         return "O"
     else:
         return "X"
-        
-def winning_conditions(fields_array):
-    global round_counter
-    #print(fields_array)
-    if fields_array[0] == "X" and fields_array[1] == "X" and fields_array[2] == "X":
-        round_counter = 10
-        return print("Contgratulations Player X won"), round_counter
-    elif fields_array[3] == "X" and fields_array[4] == "X" and fields_array[5] == "X":
-        round_counter = 10
-        return print("Contgratulations Player X won"), round_counter  
-    elif fields_array[6] == "X" and fields_array[7] == "X" and fields_array[8] == "X":
-        round_counter = 10
-        return print("Contgratulations Player X won"), round_counter
-    elif fields_array[0] == "X" and fields_array[3] == "X" and fields_array[6] == "X":
-        round_counter = 10
-        return print("Contgratulations Player X won"), round_counter
-    elif fields_array[1] == "X" and fields_array[4] == "X" and fields_array[7] == "X":
-        round_counter = 10
-        return print("Contgratulations Player X won"), round_counter
-    elif fields_array[2] == "X" and fields_array[5] == "X" and fields_array[8] == "X":
-        round_counter = 10
-        return print("Contgratulations Player X won"), round_counter
-    elif fields_array[0] == "X" and fields_array[4] == "X" and fields_array[8] == "X":
-        round_counter = 10
-        return print("Contgratulations Player X won"), round_counter
-    elif fields_array[2] == "X" and fields_array[4] == "X" and fields_array[7] == "X":
-        round_counter = 10
-        return print("Contgratulations Player X won"), round_counter
-    elif fields_array[0] == "O" and fields_array[1] == "O" and fields_array[2] == "O":
-        round_counter = 10
-        return print("Contgratulations Player O won"), round_counter
-    elif fields_array[3] == "O" and fields_array[4] == "O" and fields_array[5] == "O":
-        round_counter = 10
-        return print("Contgratulations Player O won"), round_counter  
-    elif fields_array[6] == "O" and fields_array[7] == "O" and fields_array[8] == "O":
-        round_counter = 10
-        return print("Contgratulations Player O won"), round_counter
-    elif fields_array[0] == "O" and fields_array[3] == "O" and fields_array[6] == "O":
-        round_counter = 10
-        return print("Contgratulations Player O won"), round_counter
-    elif fields_array[1] == "O" and fields_array[4] == "O" and fields_array[7] == "O":
-        round_counter = 10
-        return print("Contgratulations Player O won"), round_counter
-    elif fields_array[2] == "O" and fields_array[5] == "O" and fields_array[8] == "O":
-        round_counter = 10
-        return print("Contgratulations Player O won"), round_counter
-    elif fields_array[0] == "O" and fields_array[4] == "O" and fields_array[8] == "O":
-        round_counter = 10
-        return print("Contgratulations Player O won"), round_counter
-    elif fields_array[2] == "O" and fields_array[4] == "O" and fields_array[7] == "O":
-        round_counter = 10
-        return print("Contgratulations Player O won"), round_counter
-    elif round_counter == 9:
-        print("Its a draw")
 
-def round(fields_array, user_symbol):
-    global round_counter
-    print_current_board(fields_array)    
-    
-    if round_counter < 10:
-        while True:
-            player_move = get_player_move()
-            if fields_array[player_move] == " ":
-                fields_array[player_move] = user_symbol
-                round_counter = round_counter + 1
-                break
-            elif fields_array[player_move] == "O" or fields_array[player_move] == "X":
-                print("this square is taken, please choose another one ")
-        
-        
-        winning_conditions(fields_array)
-        print_current_board(fields_array)
-        
-    if round_counter < 10:    
-        while True:
-            ai_move = get_ai_move(fields_array)
-            fields_array[ai_move] = ai_symbol_set(user_symbol)
-            round_counter = round_counter + 1
+def check_result_winner(result):
+    if result == "XXX":
+        return "X"
+    elif result == "OOO":
+        return "O"
+    else:
+        return None
+
+def get_game_winner(fields_array):
+    #Checking rows
+    for row in range(0, 3):
+        field_array_start_idx = row * 3
+        result = fields_array[field_array_start_idx] + fields_array[field_array_start_idx + 1] + fields_array[field_array_start_idx + 2]
+        winner = check_result_winner(result)
+        if winner != None:
+            return winner
+
+    #Checking columns
+    for column in range(0, 3):
+        result = fields_array[column] + fields_array[column + 3] + fields_array[column + 6]
+        winner = check_result_winner(result)
+        if winner != None:
+            return winner
+
+    #Checking right cross
+    result = fields_array[0] + fields_array[4] + fields_array[8]
+    winner = check_result_winner(result)
+    if winner != None:
+        return winner
+
+    #Checking left cross
+    result = fields_array[2] + fields_array[4] + fields_array[6]
+    winner = check_result_winner(result)
+    if winner != None:
+        return winner
+
+    for cell in fields_array:
+        if cell == " ":
+            return None 
+    return "D"
+
+def game_round(fields_array, user_symbol):
+    while True:
+        player_move = get_player_move()
+        if fields_array[player_move] == " ":
+            fields_array[player_move] = user_symbol
             break
-        
-
-    try:    
-        print("AI moved to square {}:\n".format(ai_move))        
-    except:
-        pass
+        elif fields_array[player_move] == "O" or fields_array[player_move] == "X":
+            print("this square is taken, please choose another one ")
     
-    winning_conditions(fields_array)
+    winner = get_game_winner(fields_array)
+    if winner != None:
+        return winner
+
+    ai_move = get_ai_move(fields_array)
+    fields_array[ai_move] = get_ai_symbol(user_symbol)
+    print("AI moved to square {}: ".format(ai_move))        
+    
+    winner = get_game_winner(fields_array)
+    if winner != None:
+        return winner
+
     print_current_board(fields_array)
-    
-
-
+    return None
 
 if __name__ == '__main__':
+    #player_starts = does_player_start()
+    #print("Player {} start and his symbol is {}".format(string, user_symbol))
     
-    fields_array=[" "," "," "," "," "," "," "," "," "]
     user_symbol = circle_or_cross()
-    player_starts = does_player_start()
-    ai_move = get_ai_move(fields_array)
-    
-    round_counter = 0
-
-    if player_starts:
-        string = "does"
-    else:
-        string = "doesn't"
-    
-    print("Player {} start and his symbol is {}".format(string, user_symbol))
-
-
-    while round_counter < 10:
-        round(fields_array, user_symbol)
+    print_instruction_board()
+    fields_array = [" "," "," "," "," "," "," "," "," "]
+    winner = None
+    while winner == None:
+        winner = game_round(fields_array, user_symbol)
+        if winner != None:
+            if winner == "D":
+                print("It's a draw!")
+            else:
+                print("Player {} won!".format(winner))
+            break
+        
 
     
